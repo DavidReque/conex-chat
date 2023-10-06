481abdf168f5975f1491e7dcd7e9c26b8b363c5a
David Requeno <davidrequeno52@gmail.com>
2023-10-06 13:49:31 -0600

    middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Providers } from './Providers'
-import Provider from './components/ui/Provider'
+import Provider from '@/components/ui/Provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
