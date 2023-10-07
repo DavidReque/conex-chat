5b538fe7deb4f63dca73daba19e3ef9f3103266a
David Requeno <davidrequeno52@gmail.com>
2023-10-07 14:16:30 -0600

    user info

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -4,12 +4,15 @@ import * as React from 'react'
 
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from '@nextui-org/react'
+import { SessionProvider } from 'next-auth/react'
 
 export function Providers ({ children }: { children: React.ReactNode }) {
   // 2. Wrap NextUIProvider at the root of your app
   return (
-    <NextUIProvider>
-      {children}
-    </NextUIProvider>
+    <SessionProvider>
+      <NextUIProvider>
+        {children}
+      </NextUIProvider>
+    </SessionProvider>
   )
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,26 @@
 'use client'
 
-import { signOut } from 'next-auth/react'
+import Button from '@/components/ui/Button'
+import { signOut, useSession } from 'next-auth/react'
 
 export default function dashboard() {
+  const {data: session} = useSession()
+
   return (
     <div>
-      <button className='bg-red-400'
-      onClick={async() => {await signOut({
-        callbackUrl: '/'
-      })}}>Sign out</button>
+      {session?.user ? (
+        <div>
+          <p>{session.user.name}</p>
+          <img src={session.user.image} alt="" />
+          <button className='bg-red-400'
+        onClick={async() => {await signOut({
+          callbackUrl: '/'
+        })}}>Sign out</button>
+        </div>
+      ) : (
+        <Button/>
+      )}
+
     </div>
   )
 }
