b51e6fc4fe246e548f781635203a1647e4b54604
David Requeno <davidrequeno52@gmail.com>
2023-10-08 21:16:27 -0600

    upstash login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,50 @@
+import { db } from "@/app/lib/db";
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
+    adapter: UpstashRedisAdapter(db),
+    session: {
+        strategy: 'jwt'
+    },
+    pages: {
+        signIn: '/login'
+    },
+    
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         })
-    ]
+    ],
+    callbacks: {
+        async jwt ({token, user}){
+            const dbUser = (await db.get(`user:${token.id}`)) as User | null
+
+            if (!dbUser) {
+                token.id = user!.id
+                return token
+            }
+
+            return {
+                id: dbUser.id,
+                name: dbUser.name,
+                email: dbUser.email,
+                picture: dbUser.image
+            }
+        },
+        async session({session, token}) {
+            if (token) {
+                session.user.id = token.id
+                session.user.name = token.name
+                session.user.email = token.email
+                session.user.image = token.picture
+            }
+
+            return session
+        }
+    }
 })
 
 export {handler as GET, handler as POST};
