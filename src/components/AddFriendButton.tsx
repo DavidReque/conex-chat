03a13c414cce109594c73035ed70084ad927fe56
David Requeno <davidrequeno52@gmail.com>
2023-10-15 13:49:25 -0600

    responsive

--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -51,42 +51,46 @@ export default function AddFriendButton() {
   }
   
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="m-4">
+    <form onSubmit={handleSubmit(onSubmit)} className="mx-auto max-w-md p-4">
       <h2 className="text-2xl my-5">Add a friend by E-mail</h2>
-      <div className="w-full flex flex-col space-y-4">
-        <div className="flex flex-wrap md:flex-nowrap mb-6 md:mb-0 space-x-4">
-        <Input {...register('email')} type="email" variant="faded" label="Email" color="success" />
-          <Button
-            type="submit"
-            className="py-6"
-            color="success"
-            variant="bordered"
-            startContent={
-              <>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-16 h-16"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
-                  />
-                </svg>
-              </>
-            }
-          >
-            Add
-          </Button>
-        </div>
-        <p>{errors.email?.message}</p>
-      {showSuccesState ? (
-        <p>Friend reques sent</p>
-      ): null}
+      <div className="space-y-4">
+        <Input
+          {...register('email')}
+          type="email"
+          variant="faded"
+          label="Email"
+          color="success"
+        />
+        <Button
+          type="submit"
+          className="py-6"
+          color="success"
+          variant="bordered"
+          startContent={
+            <>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-16 h-16"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+                />
+              </svg>
+            </>
+          }
+        >
+          Add
+        </Button>
+        <p className="text-red-500">{errors.email?.message}</p>
+        {showSuccesState ? (
+          <p className="text-green-500">Friend request sent</p>
+        ) : null}
       </div>
     </form>
   );
