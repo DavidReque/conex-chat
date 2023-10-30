5c44108b35d7d6a8e4d9e8e618596f65b88af6e0
David Requeno <davidrequeno52@gmail.com>
2023-10-29 21:37:59 -0600

    chat header

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -3,6 +3,7 @@ import { db } from '@/app/lib/db'
 import { messageArrayValidator } from '@/app/lib/validations/message'
 import { fetchRedis } from '@/helpers/redis'
 import { getServerSession } from 'next-auth'
+import Image from 'next/image'
 import { notFound } from 'next/navigation'
 import React from 'react'
 
@@ -57,7 +58,31 @@ const page = async ({params}: pageProps) => {
   const initialMessages = await getChatMessages(chatId)
 
   return (
-    <div>{params.chatId}</div>
+    <div className='flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]'>
+      <div className='flex sm:items-center justify-between py-3 border-b-2 border-gray-200'>
+        <div className='relative flex items-center space-x-4'>
+          <div className='relative'>
+            <div className='relative w-8 sm:w-12 h-8 sm:h-12'>
+                <Image 
+                fill
+                referrerPolicy='no-referrer'
+                src={chatPartner.image}
+                alt={`${chatPartner.name} profile picture`}
+                className='rounded-full'
+                />
+            </div>
+          </div>
+
+          <div className='flex flex-col leading-tight'>
+            <div className='text-xl items-center'>
+              <span className='text-gray-700 mr-3 font-semibold'>{chatPartner.name}</span>
+            </div>
+
+            <span className='text-sm text-gray-600'>{chatPartner.email}</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
