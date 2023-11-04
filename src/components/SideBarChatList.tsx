616b501af009bba76394f416eecbb1ff60056e2f
David Requeno <davidrequeno52@gmail.com>
2023-11-03 22:18:45 -0600

    clg

--- src/components/SideBarChatList.tsx
+++ src/components/SideBarChatList.tsx
@@ -23,8 +23,6 @@ const SideBarChatList: FC<SideBarChatListProps> = ({friends, sessionId}) => {
        }
     }, [pathname])
 
-    console.log(friends);
-
   return (
     <ul role='list' className='max-h-[25rem] overflow-y-auto -mx-2 space-y-1'>
         {
