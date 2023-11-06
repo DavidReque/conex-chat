350bc182645de20e5a5d428d00cefc763cc94c15
David Requeno <davidrequeno52@gmail.com>
2023-11-05 20:12:48 -0600

    toast.error

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,20 +1,29 @@
 'use client'
 
+import axios from 'axios'
 import React, { FC, useRef, useState } from 'react'
+import toast from 'react-hot-toast'
 import TextareaAutosize from 'react-textarea-autosize'
 
 interface ChatInputProps {
   chatPartner: User
+  chatId: string
 }
 
-const ChatInput: FC<ChatInputProps> = ({chatPartner}) => {
+const ChatInput: FC<ChatInputProps> = ({chatPartner, chatId}) => {
 
   const textareaRef = useRef<HTMLTextAreaElement | null>(null)
   const [input, setInput] = useState<string>('')
 
-  function sendMessage() {
-    
-  }
+  async function sendMessage() {
+    try {
+      await axios.post('/api/message/send', { text: input, chatId })
+      setInput('')
+      textareaRef.current?.focus()
+    } catch (error) {
+      toast.error('Something went wrong. Please try again later.')
+    } 
+  } 
 
   return (
     <div className='border-t border-gray-200 px-4 pt-4 mb-2 sm:mb-0'>
@@ -33,6 +42,26 @@ const ChatInput: FC<ChatInputProps> = ({chatPartner}) => {
           placeholder={`Message ${chatPartner.name}`}
           className='block w-full resize-none border-0 bg-transparent text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:py-1.5 sm:leading-6'
           />
+
+          <div 
+          onClick={() => textareaRef.current?.focus()} 
+          className='py-2'
+          aria-hidden>
+              <div className='py-px'>
+                <div className='h-9'/>
+              </div>
+          </div>
+
+          <div className='absolute ring-0 bottom-0 flex justify-between py-2 pl-3 pr-2'>
+            <div className='flex-shrink-0'>
+                <button onClick={sendMessage} type='submit'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 hover:text-indigo-600">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+</svg>
+
+                </button>
+            </div>
+          </div>
       </div>
     </div>
   )
