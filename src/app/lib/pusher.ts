f74f7303bb2771569d3050676ca5c5d91dbd6c3d
David Requeno <davidrequeno52@gmail.com>
2023-11-08 21:12:06 -0600

    us2

--- src/app/lib/pusher.ts
+++ src/app/lib/pusher.ts
@@ -5,11 +5,11 @@ export const pusherServer = new PusherServer({
     appId: process.env.PUSHER_APP_ID!,
     key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
     secret: process.env.PUSHER_APP_SECRET!,
-    cluster: 'eu',
+    cluster: 'us2',
     useTLS: true
 })
 
 
 export const pusherClient = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
-    cluster: 'eu',
+    cluster: 'us2',
 });
