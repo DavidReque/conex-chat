c2c36292272327c0ba3320b5ddaafe80f2bfb612
David Requeno <davidrequeno52@gmail.com>
2023-11-13 21:41:04 -0600

    Revert "pusherclient"
    This reverts commit 719c43338b7772dc50caf5e3cca890c563c0da06.

--- src/app/lib/pusher.ts
+++ src/app/lib/pusher.ts
@@ -1,15 +1,15 @@
 import PusherServer from 'pusher'
-import PusherClient from 'pusher-js';
+import Pusher from 'pusher-js';
 
 export const pusherServer = new PusherServer({
     appId: process.env.PUSHER_APP_ID!,
     key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
     secret: process.env.PUSHER_APP_SECRET!,
-    cluster: 'us',
+    cluster: 'us2',
     useTLS: true
 })
 
 
-export const pusherClient = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
-    cluster: 'us',
+export const pusherClient = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
+    cluster: 'us2',
 });
