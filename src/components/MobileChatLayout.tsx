a7debb61b525691583b67f87dccdaa5136128799
David Requeno <davidrequeno52@gmail.com>
2023-11-18 16:26:19 -0600

    ...

--- src/components/MobileChatLayout.tsx
+++ src/components/MobileChatLayout.tsx
@@ -12,7 +12,6 @@ import { Icons } from './Icons/Icons'
 import ButtonVariants, { buttonVariants } from './ButtonVariants'
 import SideBarChatList from './SideBarChatList'
 import FriendsRequestSideBarOptions from './FriendsRequestSideBarOptions'
-import ButtonSignOut from './ui/ButtonSignOut'
 import SignOutLayout from './ui/SignOutLayout'
 
 interface MobileChatLayoutProps {
@@ -106,7 +105,7 @@ const MobileChatLayout: FC<MobileChatLayoutProps> = ({ friends, session, sidebar
                                   return (
                                     <li key={option.name}>
                                       <Link
-                                        href={option.href}
+                                        href='/dashboard/add'
                                         className='text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'>
                                         <span className='text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'>
                                           <Icon className='h-4 w-4' />
