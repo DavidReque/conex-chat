1a16f8bb4fbad7e27926dbba40ea64c7c9595724
David Requeno <davidrequeno52@gmail.com>
2023-11-19 21:54:58 -0600

    sppiner

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 import React, { FC, useRef, useState } from 'react'
 import toast from 'react-hot-toast'
 import TextareaAutosize from 'react-textarea-autosize'
+import ButtonVariants from './ButtonVariants'
 
 interface ChatInputProps {
   chatPartner: User
@@ -14,18 +15,24 @@ const ChatInput: FC<ChatInputProps> = ({chatPartner, chatId}) => {
 
   const textareaRef = useRef<HTMLTextAreaElement | null>(null)
   const [input, setInput] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   async function sendMessage() {
     if (!input) {
       return 
     }
+
+    setIsLoading(true)
+
     try {
       await axios.post('/api/message/send', { text: input, chatId })
       setInput('')
       textareaRef.current?.focus()
     } catch (error) {
       toast.error('Something went wrong. Please try again later.')
-    } 
+    } finally {
+      setIsLoading(false)
+    }
   } 
 
   return (
@@ -57,12 +64,13 @@ const ChatInput: FC<ChatInputProps> = ({chatPartner, chatId}) => {
 
           <div className='absolute ring-0 bottom-0 flex justify-between py-2 pl-3 pr-2'>
             <div className='flex-shrink-0'>
-                <button onClick={sendMessage} type='submit'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 hover:text-indigo-600">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+            <ButtonVariants className='bg-indigo-500 hover:bg-indigo-400' isLoading={isLoading} onClick={sendMessage} type='submit'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
 </svg>
 
-                </button>
+            </ButtonVariants>
+
             </div>
           </div>
       </div>
